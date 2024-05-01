586fd9e1d0b18cb4390991265cabf4d2b650e839
noname_d <davletbaev-timur@mail.ru>
2024-05-02 02:09:14 +0300

    update noname_td

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,6 +1,6 @@
 export const MAIN_ROUTE = '/'
 export const API_URL = 'https://api.meloparty.ru'
-export const WS_API_URL = 'ws://api.meloparty.ru'
+export const WS_API_URL = 'wss://api.meloparty.ru'
 
 export const ROOMS_ROUTE = '/rooms/'
 

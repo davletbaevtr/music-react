02908673608dd5cd540874856a7e0d055aa0fa4a
noname_d <davletbaev-timur@mail.ru>
2024-05-05 16:53:46 +0300

    update noname_td

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -486,7 +486,7 @@ const Room = () => {
                                                         disableEscapeKeyDown={true}
                                                     >
                                                         <DialogTitle id="alert-dialog-title">
-                                                            Отвечает {userAnswer && userAnswer.username}
+                                                            Отвечает {userAnswer.username? `${userAnswer.username}`: ''}
                                                         </DialogTitle>
                                                         <DialogContent>
                                                             <DialogContentText id="alert-dialog-description">

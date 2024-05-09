0a80db35d26bbc666c2731d55cbe0dc0d3df0cf3
noname_d <davletbaev-timur@mail.ru>
2024-05-09 12:37:10 +0300

    update noname_td

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -293,6 +293,7 @@ const Room = () => {
 
     const checkFirstPassword = () => {
         const wsUrl = `${WS_API_URL}/ws/rooms/${roomId}/?user_id=${store.user_id}&password=${password}`;
+        console.log(store.user_id);
         const ws = new WebSocket(wsUrl);
         setAudioUrl('');
 
